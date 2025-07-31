293bae28902c43f471acb10e32e2bf35782bc527
Anggita0808 <ujangcreatortv@gmail.com>
2025-07-31 08:59:06 +0700

    First commit

---
+++ app/Models/clas.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Clas extends Model
+{
+    protected $table = "clases";
+
+    protected $guarded = [];
+
+
+    public function user(){
+        return $this->hasMany(User::class, 'clas_id');
+    }
+}
