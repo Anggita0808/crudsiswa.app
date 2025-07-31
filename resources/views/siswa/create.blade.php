0c6d0297761f2130878a509c4e854d3bdd51744d
Anggita0808 <ujangcreatortv@gmail.com>
2025-07-31 12:09:57 +0700

    style(siswa):create view siswa

---
+++ resources/views/siswa/create.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Tambahkan Data siswa</h1>
+    <p>Halaman untuk menambahkan data siswa</p>
+    <from>
+        <div>
+            <table>Class Id</table>
+            <br>
+            <select nama ="kalas_id">
+                <option value="">XII PPLG 1</option>
+                <option value="">XII PPLG 2</option>
+                <option value="">XII PPLG 3</option>
+            </select>
+            </div>
+            <br>
+            <div>
+                <lable>Nama</label>
+                <br>
+                <input type="text" name="name">
+            </div>
+            <br>
+            <div>
+                <label>Nisn</label>
+                <br>
+                <input type="text" name="nisn">
+            </div>
+            <br>
+            <div>
+                <label>Alamat</label>
+                <br>
+                <input type="text" name="alamat">
+            </div>
+            <br>
+            <div>
+                <label>Email</label>
+                <br>
+                <input type="text" name="email">
+    
+            </div>
+            <br>
+            <div>
+                <label>Nomor Telepon</label>
+                <br>
+                <input type="text" name="no_handphone">
+            </div>  
+            <br>
+            <div>
+                <label>Sandi</label>
+                <br>
+                <input type="text" name="password">
+            </div>
+            <br>
+            <div>
+                <label>Foto</label>
+                <br>
+                <input type="file" name="photo">
+            </div>
+            <br>
+            <div>
+                <button type="submit">simpan</button>
+            </div>
+            <br>
+            <div>
+                <a href="/">kembali</a>
+            </div>
+        </from>
+</body>
+</html>
